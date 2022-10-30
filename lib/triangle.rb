a3b679dd5f95557da923c2c8ec0fdc23ff6da47f
Stephen <skairu09@gmail.com>
2022-10-30 07:31:25 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_accessor :length, :height, :hypotenuse
+
+  def initialize(length, height, hypotenuse)
+    @length= length
+    @height=height
+    @hypotenuse=hypotenuse
+  end
+  def kind
+
+    if length <= 0 || height <= 0 || hypotenuse <= 0
+      raise TriangleError
+    end
+
+    if length + height <= hypotenuse || length + hypotenuse <= height || height + hypotenuse <= length
+      raise TriangleError
+    end
+    if length == height && height == hypotenuse
+      :equilateral
+      elsif length == height || length == hypotenuse || height == hypotenuse
+        :isosceles
+      else
+        :scalene
+      end
+      
+
+  end
+
+class TriangleError < StandardError
+end
 end
